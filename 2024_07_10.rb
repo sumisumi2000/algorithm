b03110064c4cd6979604a4b149604d6adff4527f
川嶋澄生 <sumio.vantan308@gmail.com>
2024-07-10 22:48:31 +0900

    2024/07/10

---
+++ 2024_07_10.rb
@@ -0,0 +1,3 @@
+A, B, C, D = gets.chomp.split.map(&:to_i)
+
+puts A * B > C * D ? A * B : C * D
--- sample.rb
+++ sample.rb
@@ -1,17 +1,10 @@
-# 入力
-n, k = gets.split.map(&:to_i)
-d = gets.split(' ').map(&:to_i)
+num = 7
 
-# メソッド
-def remove_dislike_number(n, k, d)
-  all_number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  available_number = all_number - d
+answer = 0
 
-  combinations = available_number.repeated_permutation(n.to_s.length).to_a
-  combinations.each do |comb|
-    number = comb.join.to_i
-    return number if number >= n
-  end
+while num <= 7777777
+  answer += num.to_s.count('7')
+  num += 7
 end
 
-puts remove_dislike_number(n, k, d)
+p answer
