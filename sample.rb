da3fef7377fed65ccecbc95b41ce108f2b022c2b
sumisumi2000 <sumio.vantan308@gmail.com>
2024-07-29 16:40:56 +0900

    test コミット

--- sample.rb
+++ sample.rb
@@ -1,27 +1,13 @@
-# 数字の種類数
-N = gets.to_i
-# カードの配列
-cards = []
+array = (1..1000)
 
-(N * 2).times do
-  cards.push(gets.to_i)
+sum = 0
+
+array.each do |num|
+  sum += num if num % 3 == 0 || num % 5 == 0
 end
 
-# 撹拌度
-stirring_degree = 0
+puts sum
 
-cards_index = {}
+array = []
 
-cards.each_with_index do |card, idx|
-  if cards_index.key?(card)
-    cards_index[card].push(idx)
-  else
-    cards_index[card] = [idx]
-  end
-end
-
-N.times do |n|
-  stirring_degree += cards_index[n + 1][1] - cards_index[n + 1][0] - 1
-end
-
-p stirring_degree
+p *(1..1000).class
